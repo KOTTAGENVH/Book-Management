a9114621a1789be2f042a5ff420d1022bee12903
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2025-04-05 09:18:28 +0530

    fix: https-http error

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import type { Rewrite } from "next/dist/lib/load-custom-routes";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites(): Promise<Rewrite[]> {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://3.89.85.107:4000/:path*", 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
