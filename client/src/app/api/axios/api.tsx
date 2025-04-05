63f3b3cd5425b1f8585d26dc720a15f246b6c0b3
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2025-04-05 08:46:38 +0530

    Update api.tsx

--- client/src/app/api/axios/api.tsx
+++ client/src/app/api/axios/api.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 //Api Client
 const apiClient = axios.create({
-  baseURL: process.env.NEST_APP_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
